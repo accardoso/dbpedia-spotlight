622f5bc5ec9878301257928d3d52d329e13c1ee9
Alexandre Cançado Cardoso <acc.intrisic@gmail.com>
2013-11-21 14:50:45 -0200

    Authentication key removal (method and interface), and 1 more security verification

--- rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/Authentication.scala
+++ rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/Authentication.scala
@@ -12,14 +12,22 @@ import util.Random
 /**
  * Authentication class for Spotlight server.
  * Verifies if the informed api key is a valid one. If not a valid one, throws an exception informing that it was unauthorized.
- * Generates and registers new api key.
+ * Generates and registers new api key. And remove a informed key.
  *
  * @author Alexandre Cançado Cardoso - accardoso
  */
 
 object Authentication {
+
+  val configFileName: String = "./conf/server.properties"
+
   /* The path for the file containing the registered api keys */
-  val apiKeysFilePath = "rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/.api-keys" //If Spotlight is running from .../dbpedia-spotlight/ directory.
+  val apiKeysFilePath: String = getApiKeysFile(configFileName)
+
+  def getApiKeysFile(configFilePath: String): String = {
+    AuthenticationConfig.setConfigFile(configFilePath)
+    AuthenticationConfig.get("org.dbpedia.spotlight.authentication.apikeys", "rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/.api-keys")
+  }
 
   /* Public method to authenticate if it is or not a valid key */
   def authenticate(clientIp: String, key: String): Boolean = {
@@ -29,36 +37,12 @@ object Authentication {
     true
   }
 
-  /* Public method to run the new api key generation and registration interface */
-  def newKeyInterface(){
-    SpotlightLog.info(this.getClass, "**** DBpedia-Spotlight new API key generation and registration interface ****" )
-    print("Do you want to generate and register a new api key? [y|n]" )
-    val line = Console.readLine
-
-    if (line == "y"){
-      var newKey: String = ""
-      val tryTimes: Int = 10
-
-      for(i <- 1 to tryTimes){
-        newKey = generateKey()
-        if(registerKey(newKey)){
-          SpotlightLog.info(this.getClass, "The new Spotlight api key: %s", newKey)
-          return
-        }
-      }
-      SpotlightLog.info(this.getClass, "The authentication api key generator try %d times, but any new key was generated." +
-        " Try again or contact the authentication module developer for more information.", tryTimes)
-    }
-    SpotlightLog.info(this.getClass, "No api key was registered.")
-  }
-
-
   /* Verifies if the key is registered or not */
   private def isRegistered(key: String): Boolean = {
     Source.fromFile(apiKeysFilePath).getLines.foreach{
       line =>
-        if(line == key)
-          return true
+        if(line == key && line != "")
+            return true
     }
 
     //If not returned yet then it not a valid key
@@ -91,10 +75,81 @@ object Authentication {
     bigInt.toString(26)
   }
 
+  /* Remove the informed key from the api keys file */
+  private def removeKey(key: String): Boolean = {
+    //Rename the original api keys file (it is safer than rename the new file after the copy and deletion of the original file)
+    val oldFile = new File(apiKeysFilePath + ".old.tmp")
+    val apiKeyFile = new File(apiKeysFilePath) 
+    if(!apiKeyFile.renameTo(oldFile))
+      throw new IllegalAccessException("Could not remove the key, because it is unable to modify the api key file at %s".format(apiKeysFilePath))
 
-  /* Main method runs the interface for generate and register a new api key */
+    //Copy all keys except the one to be deleted from the old api key file to the new one (already with the api keys file name/path)
+    val reader = Source.fromFile(oldFile)
+    val writer = new FileWriter(apiKeyFile, true)
+    reader.getLines.foreach{
+      line =>
+        if(line != key && line != ""){
+          writer.append("\n"+line)
+          writer.flush()
+        }
+    }
+    reader.close()
+    writer.close()
+
+    //Delete the old api keys file
+    if(!oldFile.delete())
+      SpotlightLog.warn(this.getClass(), "The key was removed correctly. But could not delete the temporary file at: %s", oldFile.getCanonicalPath)
+
+  true
+  }
+
+  /* Public method to run the new api key generation and registration interface */
+  def newKeyInterface(){
+    SpotlightLog.info(this.getClass, "**** DBpedia-Spotlight new API key generation and registration interface ****" )
+    print("Do you want to generate and register a new api key? [y|n]" )
+    val line = Console.readLine
+
+    if (line == "y"){
+      var newKey: String = ""
+      val tryTimes: Int = 100
+      //Try to generate a new api key and register it (registration do not accept existing keys)
+      for(i <- 1 to tryTimes){
+        newKey = generateKey()
+        if(registerKey(newKey)){
+          //Inform the new key and return
+          SpotlightLog.info(this.getClass, "The new Spotlight api key: %s", newKey)
+          return
+        }
+      }
+      //Give up to generate a new key
+      SpotlightLog.info(this.getClass, "The authentication api key generator try %d times, but any new key was generated." +
+        " Try again or contact the authentication module developer for more information.", tryTimes)
+    }
+
+    SpotlightLog.info(this.getClass, "No api key was registered.")
+  }
+
+  /* Public method to run the safe routine to remove api key */
+  def removeKeyInterface(key: String){
+    SpotlightLog.info(this.getClass, "**** DBpedia-Spotlight API key removal interface ****" )
+    print("Do you want to remove this api key: %s ? [y|n]".format(key))
+    val line = Console.readLine
+    if (line == "y") {
+      if(removeKey(key))
+        SpotlightLog.info(this.getClass, "Successfully remove the api key: %s", key)
+      else
+        SpotlightLog.error(this.getClass, "Could not remove the api key: %s", key)
+    }else
+      SpotlightLog.info(this.getClass, "Api key removal canceled!")
+  }
+
+
+  /* Main method runs the interface for generate and register a new api key or for api key removal (if the key was informed as argument) */
   def main(args: Array[String]){
-    newKeyInterface()
+    if(args.length == 0)
+      newKeyInterface()
+    else //Assume that arg(0) is the key to be removed
+      removeKeyInterface(args(0))
   }
 
 }
--- rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/AuthenticationConfig.scala
+++ rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/AuthenticationConfig.scala
@@ -1,19 +1,29 @@
+package org.dbpedia.spotlight.web.rest.resources
+
 import java.io.{FileInputStream, File}
 import java.util.Properties
 import org.dbpedia.spotlight.exceptions.ConfigurationException
 
-class AuthenticationConfig(configFile: File) {
-  //Constructor receiving the path of the configuration file
-  def this(configFilePath: String) = this(new File(configFilePath))
-  //Constructor using the default path and default name for the configuration file (if this file do not exists it shall not work)
-  def this() = this("conf" + File.separator + "server.properties")
+object AuthenticationConfig {
 
-  if(!configFile.exists() || !configFile.isFile)
-    throw new NoSuchElementException("Could not find the configuration file: %s".format(configFile.getCanonicalPath))
+  private var configFile: File = null
+  private var properties : Properties = null
 
-  private val properties : Properties = new Properties()
-  //SpotlightLog.info(this.getClass, "Loading configuration file %s", configFile)
-  properties.load(new FileInputStream(configFile))
+  def setConfigFile(configFile: File) {
+    if(validate(configFile))
+      this.configFile = configFile
+
+    properties = new Properties()
+    properties.load(new FileInputStream(configFile))
+  }
+  def setConfigFile(configFilePath: String) { setConfigFile(new File(configFilePath)) }
+
+  private def validate(file: File):Boolean = {
+    if(!file.exists() || !file.isFile)
+      throw new NoSuchElementException("Could not find the configuration file: %s".format(configFile.getCanonicalPath))
+
+    true
+  }
 
   def get(key : String, defaultValue : String) : String = {
     properties.getProperty(key, defaultValue)
