151095053a1bfd8ce539ff041d8243182ddaf83f
Alexandre Cançado Cardoso <acc.intrisic@gmail.com>
2013-11-29 16:47:17 -0200

    Feedback parsers documentation

--- rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/FeedbackParser.scala
+++ rest/src/main/scala/org/dbpedia/spotlight/web/rest/resources/FeedbackParser.scala
@@ -2,14 +2,21 @@ package org.dbpedia.spotlight.web.rest.resources
 
 import org.dbpedia.spotlight.model.SpotlightFeedback
 
+/**
+ * This object implements a parser from a batch of feedback xml/json formatted to a list (batch) of SpotlightFeedback
+ *
+ * @author Alexandre Cançado Cardoso - accardoso
+ */
+
 object FeedbackParser{
 
+  /* Parses the informed batch of feedback in the json formatted string to a list (batch) of SpotlightFeedback */
   def fromJson(jsonFeedbackBatch: String): List[SpotlightFeedback] = {
     var parsed: List[SpotlightFeedback] = List()
-
     val result = scala.util.parsing.json.JSON.parseFull(jsonFeedbackBatch)
     var resultMap: Map[String, Any] = null
 
+    //Get the json batch root
     result match {
       // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
       case Some(map: Map[String, Any]) => resultMap = map
@@ -17,6 +24,7 @@ object FeedbackParser{
       case other => throw new IllegalArgumentException("Unknown data structure: " + other)
     }
 
+    //Parse the common fields
     val text: String = resultMap("text") match {
       case s: String => s
     }
@@ -30,9 +38,11 @@ object FeedbackParser{
       case s: String => s
     }
 
+    //Get the entities list
     val entities: List[Map[String, Any]] = resultMap("entities") match {
       case l: List[Map[String, Any]] => l
     }
+    //Parse the specific fields
     entities.foreach{ entity =>
       val entityUri:String = entity("entity_uri") match {
         case s: String => s
@@ -65,25 +75,32 @@ object FeedbackParser{
         case s: String => s
       }
 
+      //Create a new SpotlightFeedback for this entity using the common and specific fields and add it to the list of parsed feedback
       parsed = parsed :+ new SpotlightFeedback(text, docUrl, gender, entityUri, surfaceForm, offset, feedback, systemIds, manual, language)
     }
 
+    //Return all feedback of the batch as a SpotlightFeedback
     parsed
   }
 
+  /* Parses the informed batch of feedback in the xml formatted string to a list (batch) of SpotlightFeedback */
   def fromXml(xmlFeedbackBatch: String): List[SpotlightFeedback] = fromXml(scala.xml.XML.loadString(xmlFeedbackBatch))
-
+  /* Parses the informed batch of feedback in the xml (scala element) to a list (batch) of SpotlightFeedback */
   def fromXml(xmlFeedbackBatch:  scala.xml.Elem): List[SpotlightFeedback] = {
     var parsed: List[SpotlightFeedback] = List()
 
+    //Get the xml batch root
     val feedbackBatch = xmlFeedbackBatch \\ "batch"
 
+    //Parse the common fields
     val text: String = (feedbackBatch \\ "text").text.toString
     val docUrl: String = (feedbackBatch \\ "url").text.toString
     val gender:String = (feedbackBatch \\ "gender").text.toString
     val language:String = (feedbackBatch \\ "language").text.toString
 
+    //Get the entities list
     val entities = feedbackBatch \\ "entity"
+    //Parse the specific fields
     entities.foreach{ entity =>
       val entityUri:String = (entity \\ "entity_uri").text.toString
       val surfaceForm:String = (entity \\ "surface_form").text.toString
@@ -97,12 +114,13 @@ object FeedbackParser{
       val systemIds:String = (entity \\ "systems").text.toString
       val manual:String = (entity \\ "manual").text.toString
 
+      //Create a new SpotlightFeedback for this entity using the common and specific fields and add it to the list of parsed feedback
       parsed = parsed :+ new SpotlightFeedback(text, docUrl, gender, entityUri, surfaceForm, offset, feedback, systemIds, manual, language)
 
     }
 
+    //Return all feedback of the batch as a SpotlightFeedback
     parsed
   }
-
 
 }
