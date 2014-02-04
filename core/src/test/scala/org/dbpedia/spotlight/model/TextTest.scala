57d57c45bc8df497584ea6a1f3805833a5d8b71e
accardoso <acc.intrisic@gmail.com>
2014-02-03 22:30:19 -0200

    Unit text for core/model/Text.scala

---
+++ core/src/test/scala/org/dbpedia/spotlight/model/TextTest.scala
@@ -0,0 +1,42 @@
+package org.dbpedia.spotlight.model
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+/**
+ * Unit test for class org.dbpedia.spotlight.model.Text.scala, which represents a input string for spotlight.
+ *
+ * @author Alexandre Cançado Cardoso - accardoso
+ */
+
+@RunWith(classOf[JUnitRunner])
+class TextTest extends FlatSpec with ShouldMatchers {
+
+  val input: String = "Germany’s capital is Berlin.\nThe cities below are not the Brazil’s capital:\n Rio de Janeiro \t São Paulo \t Buenos Aires."
+  val expected: String = "Germany's capital is Berlin.\nThe cities below are not the Brazil's capital:\n Rio de Janeiro \t São Paulo \t Buenos Aires."
+
+  val textInstance: Text = new Text(input)
+
+  "The ADT Text" should "be constructed cleaning it up correctly" in {
+    textInstance.text.equals(expected) should be === true
+  }
+
+  "The Text instance toString" should "be 'Text[<the text attribute>]' " in {
+    textInstance.toString.equals("Text["+expected+"]") should be === true
+  }
+
+  "Two Text instance" should "be equal if and only if both text attribute are equal" in {
+    textInstance.equals(new Text(textInstance.text)) should be === true
+  }
+
+  "The Text instance hash code" should "be equal to its text attribute hash code" in {
+    textInstance.hashCode() should be === textInstance.text.hashCode
+  }
+
+  it should "be 0 (zero) if the text attribute is null" in {
+    textInstance.text = null
+    textInstance.hashCode() should be === 0
+  }
+}
