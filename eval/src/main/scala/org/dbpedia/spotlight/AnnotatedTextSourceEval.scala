42264c8ea964f9ca2d326bf51bab23e905bd23cf
accardoso <acc.intrisic@gmail.com>
2014-04-03 17:57:45 -0300

    evaluation method

--- eval/src/main/scala/org/dbpedia/spotlight/AnnotatedTextSourceEval.scala
+++ eval/src/main/scala/org/dbpedia/spotlight/AnnotatedTextSourceEval.scala
@@ -14,46 +14,73 @@ object AnnotatedTextSourceEval{
   val spotInterface: String = spotlightServer + (if(spotlightServer.endsWith("/")) "spot?text=" else "/spot?text=")
 
   def evaluate(src: AnnotatedTextSource) = {
+    var totRetrieved = 0
+    var totRelevant = 0
+    var totTP = 0
 
     var countParagraphs = 0
-    var avgPrecision = 0; var avgRecall = 0;
+    var avgPrecision = 0
+    var avgRecall = 0
+
     val parser: SpotXmlParser = new SpotXmlParser()
     val tmpFile: File = new File("AnnotatedTextSourceEval.tmp")
 
     src.foreach{ paragraph =>
       ("curl " + spotInterface +  paragraph.text.toString + " > " + tmpFile.getAbsolutePath).!
-      val ans:List[SpotOccurrence] = convertFrom(parser.extract(new Text(Source.fromFile(tmpFile).getLines().mkString(""))).asScala.toList).sorted
-      val expected:List[SpotOccurrence] = convertFrom(paragraph.occurrences).sorted
 
+      val ans:List[SpotOccurrence] =
+        convertFrom(parser.extract(new Text(Source.fromFile(tmpFile).getLines().mkString(""))).asScala.toList).sorted
+
+      val expected:List[SpotOccurrence] = convertFrom(paragraph.occurrences).sorted
       var tp: Int = 0
-      var fp: Int = 0
-      var fn: Int = 0
-      
+
+      var i: Int = 0
+      var a: SpotOccurrence = ans(i)
       expected.foreach{ e=>
-        while(e.textOffset)
-        
+        while(e.getOffset() < a.getOffset()){
+          a = ans(i)
+          i += 1
+        }
+        if(e.getOffset() == a.getOffset())
+          tp += 1
       }
 
+      val precision = if(ans.length != 0) tp / ans.length else -1
+      val recall = if(expected.length != 0) tp / expected.length else -1
+      var f1 = precision+recall
+      f1 = if(f1 > 0 && precision >= 0 && recall >= 0) 2*precision*recall / (precision+recall) else -1
 
-      val precison = tp / (tp+fp)
-      val recall = tp / (tp+fn)
+      SpotlightLog.info(this.getClass, "Paragraph %d Measures:\nPrecision = %f\nRecall = %f\nF1-score = %f",
+        precision, recall, f1)
+
+      totRetrieved += ans.length
+      totRelevant += expected.length
+      totTP += tp
 
       countParagraphs += 1
-      avgPrecision += precison
+      avgPrecision += precision
       avgRecall += recall
-
-      //Output de cada PR
-
     }
 
     if(countParagraphs == 0)
       SpotlightLog.info(this.getClass, "The informed source has no paragraphs to be evaluated.")
     else{
+      val totPrecision = if(totRetrieved != 0) totTP / totRetrieved else -1
+      val totRecall = if(totRelevant != 0) totTP / totRelevant else -1
+      var f1OfTotMeasures = totPrecision+totRecall
+      f1OfTotMeasures = if(f1OfTotMeasures > 0 && totPrecision >= 0 && totRecall >= 0)
+        2*totPrecision*totRecall / (totPrecision+totRecall) else -1
+
+      SpotlightLog.info(this.getClass, "Whole Corpus Measures:\nPrecision = %f\nRecall = %f\nF1-score = %f",
+        totPrecision, totRecall, f1OfTotMeasures)
+
       avgPrecision /= countParagraphs
       avgRecall /= countParagraphs
+      var f1OfAvgMeasures = avgPrecision+avgRecall
+      f1OfAvgMeasures = if(f1OfAvgMeasures > 0) 2*avgPrecision*avgRecall / (avgPrecision+avgRecall) else -1
 
-
-
+      SpotlightLog.info(this.getClass, "Average of Paragraphs Measures:\nPrecision = %f\nRecall = %f\nF1-score = %f",
+        avgPrecision, avgRecall, f1OfAvgMeasures)
     }
 
     if(tmpFile.exists())
@@ -84,6 +111,7 @@ object AnnotatedTextSourceEval{
 
   def main(args: Array[String]){
 
+
   }
 
 }
@@ -93,6 +121,7 @@ class SpotOccurrence(offset : Int, surfaceForm : SurfaceForm) extends Comparable
   def this(element: SurfaceFormOccurrence) = this(element.textOffset, element.surfaceForm)
 
   def compareTo(o: SpotOccurrence): Int = this.offset.compareTo(o.offset)
-  
-  def getOffset
+
+  def getOffset(): Int = offset
+  def getSurfaceForm(): SurfaceForm = surfaceForm
 }
